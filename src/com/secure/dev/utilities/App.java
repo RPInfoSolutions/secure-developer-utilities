8cf5f9e6d7a580fca52bca60e6c7fb8d00b4c78c
Hussain Gilani <hussain.gilani@icoegroup.com>
2020-09-24 03:25:21 +0530

    Implement URL encoder & Decoder

--- src/com/secure/dev/utilities/App.java
+++ src/com/secure/dev/utilities/App.java
@@ -1,12 +1,31 @@
 package com.secure.dev.utilities;
 
+import com.secure.dev.utilities.url.EncoderDecoder;
+
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class App {
     private JTextArea pasteURLTextArea;
     private JButton decodeURLButton;
     private JButton encodeURLButton;
     private JPanel mainPanel;
+
+    public App() {
+        encodeURLButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                pasteURLTextArea.setText(EncoderDecoder.encodeValue(pasteURLTextArea.getText()));
+            }
+        });
+        decodeURLButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                pasteURLTextArea.setText(EncoderDecoder.decodeValue(pasteURLTextArea.getText()));
+            }
+        });
+    }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("App");
---
+++ src/com/secure/dev/utilities/url/EncoderDecoder.java
@@ -0,0 +1,28 @@
+package com.secure.dev.utilities.url;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+public class EncoderDecoder {
+
+    public static String encodeValue(String value) {
+        try {
+            return URLEncoder.encode(value, StandardCharsets.UTF_8.toString());
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return value;
+    }
+
+    public static String decodeValue(String value) {
+        try {
+            return URLDecoder.decode(value, StandardCharsets.UTF_8.toString());
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return value;
+    }
+
+}
