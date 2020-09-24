17a81ec2ad063a8bf252282bce501a75fdad6fd0
Hussain Gilani <hussain.gilani@icoegroup.com>
2020-09-24 15:00:41 +0530

    Change to Card layout for inner panel & add base64

--- src/com/secure/dev/utilities/App.java
+++ src/com/secure/dev/utilities/App.java
@@ -1,28 +1,62 @@
 package com.secure.dev.utilities;
 
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class App {
     private JPanel mainPanel;
     private JList list1;
-    private JTextArea textArea;
+    private JTextArea urlEncoderDecoderArea;
     private JButton decodeUrlButton;
     private JButton encodeUrlButton;
+    private JPanel urlEncoderDecoder;
+    private JPanel base64EncoderDecoder;
+    private JButton encodeToBase64Button;
+    private JPanel switchPanel;
+    private JButton decodeFromBase64Button;
+    private JTextArea base64Area;
 
     public App() {
 
         decodeUrlButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                textArea.setText(EncoderDecoder.decodeUrlValue(textArea.getText()));
+                urlEncoderDecoderArea.setText(EncoderDecoder.decodeUrlValue(urlEncoderDecoderArea.getText()));
             }
         });
         encodeUrlButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                textArea.setText(EncoderDecoder.encodeUrlValue(textArea.getText()));
+                urlEncoderDecoderArea.setText(EncoderDecoder.encodeUrlValue(urlEncoderDecoderArea.getText()));
+            }
+        });
+        list1.addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                if (!e.getValueIsAdjusting()){
+                    JList source = (JList)e.getSource();
+                    String selected = source.getSelectedValue().toString();
+                    CardLayout layout = (CardLayout)switchPanel.getLayout();
+
+                    switch (selected){
+                        case "URL Encoder/Decoder": {
+                            layout.show(switchPanel, "Card1");
+                            break;
+                        }
+                        case "Base64 Encoder/Decoder": {
+                            layout.show(switchPanel, "Card2");
+                            break;
+                        }
+                        default: {
+                            /*urlEncoderDecoder.setVisible(false);*/
+                        }
+                    }
+
+                }
             }
         });
     }
